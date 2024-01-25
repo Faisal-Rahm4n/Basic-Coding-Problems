4511feb5bad3e53479b8c99365300868597122e0
Faisal Rahman <Faisalrahman6026@gmail.com>
2024-01-25 23:26:32 +0600

    initial commit

---
+++ PassWordGenerator.py
@@ -0,0 +1,16 @@
+import string
+import random
+
+def gen():
+        s1 = string.ascii_uppercase
+
+        s2 = string.ascii_lowercase
+
+        s4 = string.digits
+
+        s3 = string.punctuation
+
+        passlen = int(input("Enter The"))
+
+
+gen()
