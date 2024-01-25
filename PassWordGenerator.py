57312095a45aeb96199a55022a1a2b23aa3037f7
Faisal Rahman <Faisalrahman6026@gmail.com>
2024-01-25 23:32:37 +0600

    initial

--- PassWordGenerator.py
+++ PassWordGenerator.py
@@ -10,7 +10,18 @@ def gen():
 
         s3 = string.punctuation
 
-        passlen = int(input("Enter The"))
+        passlen = int(input("Enter The Password length\n"))
 
+        s=[]
+        s.extend(list(s1))
+        s.extend(list(s2))
+        s.extend(list(s3))
+        s.extend(list(s4))
+
+
+        random.shuffle(s)
+
+        pas = ("".join(s[0:passlen]))
+        print(pas)
 
 gen()
